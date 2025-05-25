48d312a1b11d1514ed14edcace3ecb31421f74a4
hermer29 <kliman.chan@gmail.com>
2025-05-26 01:04:31 +0300

    fix tests and main

--- src/authentication.cpp
+++ src/authentication.cpp
@@ -3,23 +3,12 @@
 #include <fmt/format.h>
 #include <jwt-cpp/jwt.h>
 
-#include <userver/server/handlers/http_handler_base.hpp>
-
 namespace passec {
 
-class Authentication final : public userver::server::handlers::HttpHandlerBase {
-public:
-  static constexpr std::string_view kName = "handler-auth";
-
-  using HttpHandlerBase::HttpHandlerBase;
-
-  std::string HandleRequestThrow(
-      const userver::server::http::HttpRequest &request,
-      userver::server::request::RequestContext &) const override {
-    return passec::Authorize(request.GetArg("login"), request.GetArg("password"));
-  }
-  
-};
+std::string Authentication::HandleRequestThrow(const userver::server::http::HttpRequest &request,
+      userver::server::request::RequestContext &) const {
+  return passec::Authorize(request.GetArg("login"), request.GetArg("password"));
+}
 
 std::string Authorize(std::string_view login, std::string_view password) {
   return fmt::format("Hello, {} {}!\n", login, password);
--- src/authentication.hpp
+++ src/authentication.hpp
@@ -7,9 +7,17 @@
 
 namespace passec {
 
-    class Authentication final : public userver::server::handlers::HttpHandlerBase {
+class Authentication final : public userver::server::handlers::HttpHandlerBase {
+public:
+  static constexpr std::string_view kName = "handler-auth";
 
-    };
+  using HttpHandlerBase::HttpHandlerBase;
+
+  std::string HandleRequestThrow(
+      const userver::server::http::HttpRequest &request,
+      userver::server::request::RequestContext &) const override;
+  
+};
 
 std::string Authorize(std::string_view login, std::string_view password);
 
